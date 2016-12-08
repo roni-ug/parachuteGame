8eec3d2116350c9b60edda854d2f9e76ad5ac449
Rony Yahel <roni.ug@gmail.com>
2016-12-08 12:04:05 +0200

    small changes and bug fix

--- WebContent/Classes.js
+++ WebContent/Classes.js
@@ -1,111 +1,111 @@
 
 //stores all game elements
-class ElementsData {
-	constructor(){
-	this.backGroundScreen = new imageElement(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT,"background");
+class GameElements {
+	constructor() {
+		this.backGroundScreen = new ImageElement(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, "background");
 
-    this.boat = new imageElement(Math.floor(CANVAS_WIDTH/2) -60 , 400, 140, 160,"boat");
-  	this.plane = new Plane(CANVAS_WIDTH, 30, 100,  60,"plane");
+		this.boat = new ImageElement(Math.floor(CANVAS_WIDTH / 2) - 60, 400, 140, 160, "boat");
+		this.plane = new Plane(CANVAS_WIDTH, 30, 100, 60, "plane");
 
-	this.scoreBoxText = new textElement(30, 30, 100, 100, `score:${SCORE}         lives:`);
+		this.scoreBoxText = new TextElement(30, 30, 100, 100, `score:${SCORE}         lives:`);
 
-	this.allHearts = new AllHearts("lives");
-	this.allParachutes = new AllParachutes("parachute");
+		this.allHearts = new AllHearts("lives");
+		this.allParachutes = new AllParachutes("parachute");
 
-	this.newGameButton = new imageElement(250, 300, 300, 125,"newGameButton");
-	this.newGameButton.appearsInScreen = OPENNING_SCREEN;
+		this.newGameButton = new ImageElement(250, 300, 300, 125, "newGameButton");
+		this.newGameButton.appearsInScreen = OPENNING_SCREEN;
 
-	this.parachuteImageOfText = new imageElement(170, 100, 515, 85,"parachuteText");
-	this.parachuteImageOfText.appearsInScreen = OPENNING_SCREEN;
+		this.parachuteImageOfText = new ImageElement(170, 100, 515, 85, "parachuteText");
+		this.parachuteImageOfText.appearsInScreen = OPENNING_SCREEN;
 
-	this.gameOverImageOfText = new imageElement(170, 100, 505, 85,"gameOverText");
-	this.gameOverImageOfText.appearsInScreen = END_SCREEN;
-	}	
+		this.gameOverImageOfText = new ImageElement(170, 100, 505, 85, "gameOverText");
+		this.gameOverImageOfText.appearsInScreen = END_SCREEN;
+	}
 }
 
 
 // the general screen element that image and text inherit from
 class generalElement {
-	constructor(objX, objY, objWidth, objHeight){
+	constructor(objX, objY, objWidth, objHeight) {
 		this.x = objX, this.y = objY;
 		this.width = objWidth, this.height = objHeight;
 		this.appearsInScreen = MAIN_GAME_SCREEN;
 	}
 
-	draw(){};
-	update(){};
+	draw() { };
+	update() { };
 }
 
 
-class imageElement extends generalElement {
-	constructor(objX, objY, objWidth, objHeight,imgName){
+class ImageElement extends generalElement {
+	constructor(objX, objY, objWidth, objHeight, imgName) {
 		super(objX, objY, objWidth, objHeight);
-		this.sprite = new sprite(imgName);
-
+		this.sprite = new sprite(this, imgName);
+		
 	}
-	draw(){
+	draw() {
 		this.sprite.draw(canvas, this.x, this.y);
-	}	
+	}
 }
 
-class textElement extends generalElement {
-	constructor (objX, objY, objWidth, objHeight, text){
+class TextElement extends generalElement {
+	constructor(objX, objY, objWidth, objHeight, text) {
 		super(objX, objY, objWidth, objHeight);
 		this.text = text;
 		canvas.font = "15px Ariel";
-	}	
-	draw (){
+	}
+	draw() {
 		canvas.fillText(this.text, this.x, this.y);
 	}
 }
 
 
 
-class Plane extends imageElement{
-	constructor(objX, objY, objWidth, objHeight,imgName){
-		super(objX, objY, objWidth, objHeight,imgName);
+class Plane extends ImageElement {
+	constructor(objX, objY, objWidth, objHeight, imgName) {
+		super(objX, objY, objWidth, objHeight, imgName);
 	}
-	update(){
+	update() {
 		if (this.x > -this.width) {
 			this.x -= PLANE_SPEED;
-		} 
+		}
 		else {
 			this.x = CANVAS_WIDTH;
-		}	
-	}		
+		}
+	}
 }
 
-class AllHearts{
-	constructor(imgName){
+class AllHearts {
+	constructor(imgName) {
 		this.heartElements = [];
-        this.appearsInScreen = MAIN_GAME_SCREEN;
-		
-		for ( let i = 0; i < MAX_LIVES; i++) {
-			this.heartElements.push(new imageElement(160 + 35 * i, 15, 30, 30,imgName));
+		this.appearsInScreen = MAIN_GAME_SCREEN;
+
+		for (let i = 0; i < MAX_LIVES; i++) {
+			this.heartElements.push(new ImageElement(160 + 35 * i, 15, 30, 30, imgName));
 		}
-		
+
 	}
-	
-	
-	draw (){
-        this.heartElements.forEach(function (element) {
-            element.draw();
-        });      
-}
-	
-	remove (){
+
+
+	draw() {
+		this.heartElements.forEach(function (element) {
+			element.draw();
+		});
+	}
+
+	remove() {
 		if (this.heartElements.length > 0) {
 			this.heartElements.pop();
 		}
 	}
-	
-	update(){}
+
+	update() { }
 }
 
 
-class AllParachutes{
-    
-	constructor(imgName){
+class AllParachutes {
+
+	constructor(imgName) {
 		this.appearsInScreen = MAIN_GAME_SCREEN;
 		this.update_counter = 0;
 		this.rand = Math.round(Math.random() * DROP_RATE);
@@ -113,87 +113,90 @@ class AllParachutes{
 		this.key = 0;
 		this.img = imgName;
 	}
-	
-	
-	update(){
+
+
+	update() {
 		// handles pushing new parachute to parachute array in random times
 		if (this.update_counter === this.rand) {
 			this.update_counter = 0;
 			this.rand = Math.round(Math.random() * DROP_RATE + 100);
-			if (Object.keys(this.parachuteElements).length < 2) {
+			if (Object.keys(this.parachuteElements).length < 2 && gameElements.plane.x > MARGIN && gameElements.plane.x < CANVAS_WIDTH - MARGIN) {
 				this.key++;
 				this.addParachute();
 			}
 		}
 		this.update_counter++;
 		var self = this;
-		
-		Object.keys(this.parachuteElements).forEach( function (currKey) {
-			self.parachuteElements[currKey].update();	
+
+		Object.keys(this.parachuteElements).forEach(function (currKey) {
+			self.parachuteElements[currKey].update();
 		});
-        
+
 	}
-	
-	addParachute (){
-        
-		this.parachuteElements[this.key] = new Parachute(elementsData.plane.x, 30, 60, 60,this.img, this.key);
+
+	addParachute() {
+
+		this.parachuteElements[this.key] = new Parachute(gameElements.plane.x, 30, 60, 60, this.img, this.key);
 	}
-	remove(key){
+	remove(key) {
 		delete this.parachuteElements[key];
 	}
-	draw (){
+	draw() {
 		var self = this;
-		Object.keys(this.parachuteElements).forEach( function (currKey) {
+		Object.keys(this.parachuteElements).forEach(function (currKey) {
 			let isLoaded = self.parachuteElements[currKey].sprite.image.complete;
-			if(isLoaded ){
-				self.parachuteElements[currKey].draw();	
+			if (isLoaded) {
+				self.parachuteElements[currKey].draw();
 			}
-			
-		});	
-    }
+
+		});
+	}
 }
 
 
-class Parachute extends imageElement{
-	constructor	(objX, objY, objWidth, objHeight,imgName,key){
-		super(objX, objY, objWidth, objHeight,imgName);
-		this.key= key;
+class Parachute extends ImageElement {
+	constructor(objX, objY, objWidth, objHeight, imgName, key) {
+		super(objX, objY, objWidth, objHeight, imgName);
+		this.key = key;
+		this.counter = 0;
 	}
-	update(){
+	update() {
 		this.y += DROP_SPEED;
-		if (this.y >= elementsData.boat.y && 
-				this.y < elementsData.boat.y + DROP_SPEED) {
-				
-				if (this.x >= elementsData.boat.x && 
-					this.x <= elementsData.boat.x + elementsData.boat.width) {
-					
-					SCORE++;
-					elementsData.scoreBoxText.text = "score:" + SCORE + "         lives:";
-				} 
-				else {
-					LIVES--;
-					elementsData.allHearts.remove();
-					
-					if (LIVES === 0) {
-						GAME_ACTIVE = false;
-					}
-				}
-				elementsData.allParachutes.remove(this.key);
+		if (this.y >= gameElements.boat.y &&
+			this.y < gameElements.boat.y + DROP_SPEED) {
+
+			if (this.x >= gameElements.boat.x &&
+				this.x <= gameElements.boat.x + gameElements.boat.width) {
+
+				SCORE++;
+				gameElements.scoreBoxText.text = "score:" + SCORE + "         lives:";
 			}
+			else {
+				LIVES--;
+				gameElements.allHearts.remove();
+
+				if (LIVES === 0) {
+					GAME_ACTIVE = false;
+				}
+			}
+			gameElements.allParachutes.remove(this.key);
+		}
 	}
 }
 
 class sprite {
-	constructor(name){
+	constructor(ob, name) {
 		this.image = new Image();
-		this.image.id = name;
-		var self = this;
-		
+
 
 		this.spriteImagePath = "images/";
+
 		this.image.src = this.spriteImagePath + name + ".png";
+
+
 	}
 	draw(canvas, x, y) {
+
 		canvas.drawImage(this.image, x, y);
 	}
 }
--- WebContent/ParachuteGame.js
+++ WebContent/ParachuteGame.js
@@ -9,8 +9,9 @@ const DROP_RATE = 300;
 const OPENNING_SCREEN = 1;
 const MAIN_GAME_SCREEN = 2;
 const END_SCREEN = 3;
-const leftArrow = '37';
-const rightArrow = '39';
+const LEFTARROW = '37';
+const RIGHTARROW = '39';
+const MARGIN  = 80;
 
 let GAME_ACTIVE = false;
 
@@ -24,9 +25,9 @@ const canvas = document.getElementById("canvas1").getContext("2d");
 
 
 
-// elementsData is an instance of the class ElementsData that stores the
+// gameElements is an instance of the class GameElements that stores the
 // multiple game elements, such as boat, parachute etc..
-let elementsData = new ElementsData();
+let gameElements = new GameElements();
 initializeGame();
 showGameOpeningScreen();
 document.onclick = handleGameResponseToMouseClick;
@@ -35,10 +36,10 @@ document.onkeydown = hadlePlayerResponseToKeyPressed;
 
 
 function showGameOpeningScreen() {
-	elementsData.newGameButton.sprite.image.onload = function() {
-		elementsData.backGroundScreen.draw();
-		elementsData.parachuteImageOfText.draw();
-		elementsData.newGameButton.draw();
+	gameElements.newGameButton.sprite.image.onload = function() {
+		gameElements.backGroundScreen.draw();
+		gameElements.parachuteImageOfText.draw();
+		gameElements.newGameButton.draw();
 	};
 
 };
@@ -64,23 +65,23 @@ function gameLoop() {
 };
 
 function showGameEndedScreen() {
-	elementsData.backGroundScreen.draw();
-	elementsData.gameOverImageOfText.draw();
-	elementsData.newGameButton.draw();
+	gameElements.backGroundScreen.draw();
+	gameElements.gameOverImageOfText.draw();
+	gameElements.newGameButton.draw();
 };
 
 
 // updates the movements and status of elements on screen
 function update() {
-	Object.keys(elementsData).forEach( function (key) {
-		elementsData[key].update();	
+	Object.keys(gameElements).forEach( function (key) {
+		gameElements[key].update();	
 	});
 };
 
 function draw(){
-	Object.keys(elementsData).forEach( function (key) {
-		if(elementsData[key].appearsInScreen == MAIN_GAME_SCREEN){
-			elementsData[key].draw();	
+	Object.keys(gameElements).forEach( function (key) {
+		if(gameElements[key].appearsInScreen == MAIN_GAME_SCREEN){
+			gameElements[key].draw();	
 		}		
 	});
 };
@@ -92,17 +93,17 @@ function handleGameResponseToMouseClick(event) {
 
 	let canvasX = event.pageX;
 	let canvasY = event.pageY;
-
+	let ClickedInButtonBoundsX = (canvasX >= gameElements.newGameButton.x &&  
+		canvasX <= gameElements.newGameButton.x + gameElements.newGameButton.width);
+	let ClickedInButtonBoundsY = (canvasY >= gameElements.newGameButton.y && 
+		canvasY <= gameElements.newGameButton.y + gameElements.newGameButton.height);
+	
 	if (!GAME_ACTIVE) {
 		//responds to pressing the new game button
-		if ((canvasX >= elementsData.newGameButton.x && 
-			canvasX <= elementsData.newGameButton.x + elementsData.newGameButton.width) && 
-			(canvasY >= elementsData.newGameButton.y && 
-			canvasY <= elementsData.newGameButton.y + elementsData.newGameButton.height)) {
-
+		if (ClickedInButtonBoundsX && ClickedInButtonBoundsY) {
 			GAME_ACTIVE = true;
 			initializeGame();
-			elementsData = new ElementsData();
+			gameElements = new GameElements();
 			refreshIntervalId = setInterval(gameLoop, 1000 / FPS);
 		}
 	}
@@ -117,16 +118,16 @@ function hadlePlayerResponseToKeyPressed(keyPressedEvent) {
 	
 	if (GAME_ACTIVE) {
 		// left arrow
-		if (keyPressedEvent.keyCode === leftArrow) {
-			elementsData.boat.x -= BOAT_SPEED;
+		if (keyPressedEvent.keyCode == LEFTARROW) {
+			gameElements.boat.x -= BOAT_SPEED;
 
 		}
 		// right arrow
-		else if (keyPressedEvent.keyCode === rightArrow) {
-			elementsData.boat.x += BOAT_SPEED;
+		else if (keyPressedEvent.keyCode == RIGHTARROW) {
+			gameElements.boat.x += BOAT_SPEED;
 		}
 		// makes sure the boat won't exit screen bounds
-		elementsData.boat.x = Math.min(Math.max(elementsData.boat.x, 0), CANVAS_WIDTH - elementsData.boat.width);
+		gameElements.boat.x = Math.min(Math.max(gameElements.boat.x, 0), CANVAS_WIDTH - gameElements.boat.width);
 	}
 
 };
